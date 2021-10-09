2b296189833e64e2630ab53cc9c7bc4a86040184
micha vie <micha@freeiam.com>
2021-10-09 18:03:55 +0200

    update tailwind purge list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './features/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false,
   theme: {
     extend: {
