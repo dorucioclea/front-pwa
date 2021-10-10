743356b7e0d4b41ac58bce057ad78ad41757cfed
micha vie <micha@freeiam.com>
2021-10-10 04:02:20 +0200

    update navigation

--- features/Layout/Navigation.tsx
+++ features/Layout/Navigation.tsx
@@ -3,11 +3,12 @@ import { Config } from '../../config'
 type Props = {}
 
 const Navigation = ({}: Props) => (
-  <header className="bg-black p-4 w-full">
-    <div className="flex">
+  <header className="bg-black py-2 px-8 w-full">
+    <div className="flex items-center">
+      <img src="/images/logo-white.png" className="block mr-2 w-12 h-12" />
       <a
         href="/"
-        className="font-head text-white text-xl uppercase tracking-wider"
+        className="font-head text-white text-2xl uppercase tracking-widest"
       >
         {Config.App.Name}
       </a>
