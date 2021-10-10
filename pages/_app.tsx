620c6e485b1fb792578fffdd40efce649d456e88
micha vie <micha@freeiam.com>
2021-10-10 09:26:45 +0200

    add apple touch icon link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,13 +15,18 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="theme-color" content={Config.App.Colors.Teal} />
         <meta name="description" content={Config.App.Description} />
 
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/icons/icon-192x192.png"
+        />
 
         <title>{title}</title>
       </Head>
