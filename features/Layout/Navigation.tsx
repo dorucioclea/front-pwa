bc2425ce8580e04e9b312f60c339d58a933c1d72
micha vie <micha@freeiam.com>
2021-10-14 17:22:52 +0200

    fix navigation logo linking

--- features/Layout/Navigation.tsx
+++ features/Layout/Navigation.tsx
@@ -4,15 +4,10 @@ type Props = {}
 
 const Navigation = ({}: Props) => (
   <header className="bg-black py-2 px-8 w-full">
-    <div className="flex items-center">
+    <a href="/" className="flex items-center">
       <img src="/images/logo-white.png" className="block mr-2 w-12 h-12" />
-      <a
-        href="/"
-        className="font-head text-white text-2xl uppercase tracking-widest"
-      >
-        {Config.App.Name}
-      </a>
-    </div>
+      <span className="font-head text-white text-2xl uppercase tracking-widest">{Config.App.Name}</span>
+    </a>
   </header>
 )
 
