49bc1c725a3af3c978bef0195a18ffb10a35ba72
micha vie <micha@freeiam.com>
2021-10-18 00:47:20 +0200

    fix build with erdjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: require.resolve('browserify-fs'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        path: require.resolve('path-browserify'),
+      }
+    }
+
+    return config
+  },
 }
