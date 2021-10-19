198d3fbcbf19978d9e04e49c717b9fed3b7ba378
micha vie <micha@freeiam.com>
2021-10-19 19:25:08 +0200

    fix apple icon path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,12 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const title = pageProps.title
-    ? `${pageProps.title} - ${Config.App.Name}`
-    : Config.App.Name
+  const title = pageProps.title ? `${pageProps.title} - ${Config.App.Name}` : Config.App.Name
 
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="theme-color" content={Config.App.Colors.Teal} />
@@ -22,11 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/icons/icon-192x192.png"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon-192x192.png" />
 
         <title>{title}</title>
       </Head>
