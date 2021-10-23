c3d5748c50d0bfb8023e5f2506e38ef7719fa586
micha vie <micha@freeiam.com>
2021-10-23 21:40:09 +0200

    update navigation

--- features/Layout/Header.tsx
+++ features/Layout/Header.tsx
@@ -1,18 +1,23 @@
 import { Config } from '../../config'
 import { getHttpService } from '../http'
-import { ConnectButton, DisconnectButton, handleAppResponse, Navigation } from '@superciety/pwa-core-library'
 import { getAuthProofableToken, storeAuthVerifyRequest } from '../User/api'
-import { ProofableLogin } from '@superciety/pwa-core-library/lib/services/auth'
 import { useAppDispatch, useAppSelector } from '../store'
 import { login, logout } from '../User/store/slice'
 import { selectUserLoggedIn } from '../User/store/selectors'
+import {
+  ConnectButton,
+  DisconnectButton,
+  handleAppResponse,
+  Navigation,
+  NavigationMobile,
+  ProofableLogin,
+} from '@superciety/pwa-core-library'
 
-type Props = {}
-
-const Header = ({}: Props) => {
+const Header = () => {
   const httpService = getHttpService()
   const dispatch = useAppDispatch()
   const isLoggedIn = useAppSelector(selectUserLoggedIn)
+  const navItems = isLoggedIn ? Config.Navigation.Authenticated : Config.Navigation.Guest
 
   const handleProofableTokenRequest = async () => (await getAuthProofableToken(httpService)).data.token
 
@@ -22,12 +27,12 @@ const Header = ({}: Props) => {
   const handleLogoutRequest = () => dispatch(logout())
 
   return (
-    <header className="bg-black py-3 px-8 w-full flex justify-between items-center">
+    <header className="bg-black py-3 px-2 md:px-8 w-full flex justify-between items-center">
       <a href="/" className="flex items-center">
-        <img src="/images/logo-white.png" className="block mr-2 w-12 h-12" />
+        <img src="/images/logo-white.png" className="block mr-1 md:mr-2 w-12 h-12" />
         <span className="font-head text-white text-2xl uppercase tracking-widest">{Config.App.Name}</span>
       </a>
-      <Navigation items={isLoggedIn ? Config.Navigation.Authenticated : Config.Navigation.Guest} />
+      <Navigation items={navItems} className="hidden md:block" />
       <div>
         {isLoggedIn ? (
           <DisconnectButton onClick={handleLogoutRequest} />
@@ -35,6 +40,7 @@ const Header = ({}: Props) => {
           <ConnectButton onTokenRequest={handleProofableTokenRequest} onLocalLogin={handleProofableLogin} />
         )}
       </div>
+      <NavigationMobile items={navItems} />
     </header>
   )
 }
