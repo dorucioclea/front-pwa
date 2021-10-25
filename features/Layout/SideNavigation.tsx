f5d782e62d96bab04bfe0e33fa54a4d26fd31a52
micha vie <micha@freeiam.com>
2021-10-25 19:57:24 +0200

    update side-navigation

--- features/Layout/SideNavigation.tsx
+++ features/Layout/SideNavigation.tsx
@@ -21,31 +21,35 @@ const SideNavigation = ({ items, heading, className }: Props) => {
   const router = useRouter()
   const isActive = useCallback((item: SideNavigationItem) => item.href === router.asPath, [router.asPath])
 
-  const linkColorClassName = (item: SideNavigationItem) =>
-    isActive(item) ? `bg-gray-100 ${item.colorClassName}` : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
+  const linkDynamicClassNames = (item: SideNavigationItem) =>
+    isActive(item)
+      ? `bg-gray-100 font-bold ${item.colorClassName}`
+      : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
 
   const iconColorClassName = (item: SideNavigationItem) =>
     isActive(item) ? `text-gray-500 ${item.colorClassName}` : 'text-gray-400 group-hover:text-gray-500'
 
   return (
     <>
-      {heading && <h2 className="font-text text-gray-500 text-lg pl-1 mb-2">{heading}</h2>}
-      <nav className={`space-y-2 ${className || ''}`} aria-label="Sidebar">
-        {items.map((item) => (
+      {heading && <h2 className="text-gray-500 text-lg pl-1 mb-2">{heading}</h2>}
+      <nav className={`space-y-2 text-lg ${className || ''}`} aria-label="Sidebar">
+        {items.map(item => (
           <Link href={item.href}>
             <a
               key={item.name}
-              className={`group flex items-center px-3 py-2 text-sm font-medium rounded-lg ${linkColorClassName(item)}`}
+              className={`group flex items-center px-3 py-2 rounded-lg ${linkDynamicClassNames(item)}`}
               aria-current={isActive(item) ? 'page' : undefined}
             >
               {item.icon && (
-                <FontAwesomeIcon
-                  icon={item.icon}
-                  aria-hidden="true"
-                  className={`flex-shrink-0 -ml-1 mr-3 h-6 w-6 ${iconColorClassName(item)}`}
-                />
+                <span className="inline-flex justify-center w-10">
+                  <FontAwesomeIcon
+                    icon={item.icon}
+                    aria-hidden="true"
+                    className={`flex-shrink-0 -ml-1 mr-3 h-6 w-6 ${iconColorClassName(item)}`}
+                  />
+                </span>
               )}
-              <span className="truncate">{item.name}</span>
+              <span className="tracking-wide">{item.name}</span>
             </a>
           </Link>
         ))}
