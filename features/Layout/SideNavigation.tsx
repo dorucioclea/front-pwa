4dabc030673a72679aaf9ee686d4ea7458733836
micha vie <micha@freeiam.com>
2021-10-28 08:17:18 +0200

    fix side navigation react key warning

--- features/Layout/SideNavigation.tsx
+++ features/Layout/SideNavigation.tsx
@@ -22,9 +22,7 @@ const SideNavigation = ({ items, heading, className }: Props) => {
   const isActive = useCallback((item: SideNavigationItem) => item.href === router.asPath, [router.asPath])
 
   const linkDynamicClassNames = (item: SideNavigationItem) =>
-    isActive(item)
-      ? `bg-gray-100 font-bold ${item.colorClassName}`
-      : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
+    isActive(item) ? `bg-gray-100 ${item.colorClassName}` : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
 
   const iconColorClassName = (item: SideNavigationItem) =>
     isActive(item) ? `text-gray-500 ${item.colorClassName}` : 'text-gray-400 group-hover:text-gray-500'
@@ -34,9 +32,8 @@ const SideNavigation = ({ items, heading, className }: Props) => {
       {heading && <h2 className="text-gray-500 text-lg pl-1 mb-2">{heading}</h2>}
       <nav className={`space-y-2 text-lg ${className || ''}`} aria-label="Sidebar">
         {items.map(item => (
-          <Link href={item.href}>
+          <Link key={item.name} href={item.href}>
             <a
-              key={item.name}
               className={`group flex items-center px-3 py-2 rounded-lg ${linkDynamicClassNames(item)}`}
               aria-current={isActive(item) ? 'page' : undefined}
             >
