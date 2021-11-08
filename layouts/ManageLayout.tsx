e9cd6f240302cebea32d7e9f2b9ffc4cb2024191
micha vie <micha@freeiam.com>
2021-11-08 01:51:49 +0100

    disable manage webhooks in sidebar

--- layouts/ManageLayout.tsx
+++ layouts/ManageLayout.tsx
@@ -1,7 +1,7 @@
 import Header from '../features/Layout/Header'
 import Footer from '../features/Layout/Footer'
 import { SideNavigation, SideNavigationItem } from '@superciety/pwa-core-library'
-import { faHandsHelping, faHome, faIdBadge, faWaveSquare } from '@fortawesome/free-solid-svg-icons'
+import { faHandsHelping, faHome, faIdBadge } from '@fortawesome/free-solid-svg-icons'
 import { Config } from '../config'
 
 type Props = {
@@ -31,14 +31,15 @@ const ManageLayout = (props: Props) => {
     },
   ]
 
-  const sideNavigationDevelopers: SideNavigationItem[] = [
-    {
-      text: 'Webhooks',
-      href: Config.App.Pages.ManageWebhooks,
-      icon: faWaveSquare,
-      colorClassName: 'text-gray-900',
-    },
-  ]
+  // COMING SOON
+  // const sideNavigationDevelopers: SideNavigationItem[] = [
+  //   {
+  //     text: 'Webhooks',
+  //     href: Config.App.Pages.ManageWebhooks,
+  //     icon: faWaveSquare,
+  //     colorClassName: 'text-gray-900',
+  //   },
+  // ]
 
   return (
     <div className="overflow-hidden pb-24 mb:pb-0">
@@ -46,7 +47,7 @@ const ManageLayout = (props: Props) => {
       <div className="flex flex-wrap max-w-8xl mx-auto md:pt-12 mb-20 md:mb-40">
         <div className="w-full md:w-1/3 lg:w-1/4 p-4">
           <SideNavigation items={sideNavigation} className="mb-4" />
-          <SideNavigation heading="Developers" items={sideNavigationDevelopers} />
+          {/* <SideNavigation heading="Developers" items={sideNavigationDevelopers} /> */}
         </div>
         <main className="w-full md:w-2/3 lg:w-3/4 px-4 pt-8 md:pt-4">
           {props.heading && <h1 className="text-4xl mb-2">{props.heading}</h1>}
