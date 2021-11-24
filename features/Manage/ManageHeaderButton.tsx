9c654de9cb08c2f9a6060d7c141080ce9e3f4716
micha vie <micha@freeiam.com>
2021-11-24 22:41:09 +0100

    update manage header button

--- features/Manage/ManageHeaderButton.tsx
+++ features/Manage/ManageHeaderButton.tsx
@@ -5,8 +5,8 @@ import { Config } from '../../config'
 
 const ManageHeaderButton = () => (
   <Link href={Config.App.Pages.Manage}>
-    <a className="flex justify-center items-center h-8 w-8 rounded-full hover:shadow-2xl group bg-gray-600">
-      <FontAwesomeIcon icon={faUser} size="1x" className="text-gray-800 group-hover:text-primary-400" />
+    <a className="flex justify-center items-center h-8 w-8 rounded-full hover:shadow-2xl group bg-gray-600 hover:bg-primary-400">
+      <FontAwesomeIcon icon={faUser} size="1x" className="text-gray-800 group-hover:text-white" />
     </a>
   </Link>
 )
