4014b4589bec67fdbd65014411b3fabd7d62dbd9
micha vie <micha@freeiam.com>
2021-11-26 14:50:17 +0100

    update identity connect button connected styles

--- features/Manage/identity/_ConnectButton.tsx
+++ features/Manage/identity/_ConnectButton.tsx
@@ -1,6 +1,7 @@
 import { IconDefinition } from '@fortawesome/fontawesome-common-types'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { classNames } from '@superciety/pwa-core-library'
 
 type Props = {
   children: any
@@ -13,27 +14,29 @@ type Props = {
 
 const _ConnectButton = (props: Props) => {
   const overlayClassNames =
-    'opacity-0 group-hover:opacity-100 absolute inset-0 flex justify-center items-center bg-gray-200 rounded-xl transition duration-300 text-gray-600'
+    'opacity-0 group-hover:opacity-100 absolute inset-0 flex justify-center items-center rounded-xl transition duration-300 text-white'
 
-  const Overlay = () => <span className={overlayClassNames}>{props.connected ? 'Disconnect' : 'Connect'}</span>
+  const Overlay = () => (
+    <span className={classNames(overlayClassNames, props.connected ? 'bg-red-500' : 'bg-green-500')}>
+      {props.connected ? 'Disconnect' : 'Connect'}
+    </span>
+  )
 
   const Icon = ({ icon }: { icon: IconDefinition }) => (
     <FontAwesomeIcon icon={icon} size="lg" className="text-white opacity-75 inline-block mr-3" />
   )
 
-  const ConnectedIcon = () => <FontAwesomeIcon icon={faCheck} size="lg" className="text-green-500" />
+  const ConnectedIcon = () => <FontAwesomeIcon icon={faCheck} size="lg" className="text-green-400" />
 
   return (
     <a
       href={props.href}
       onClick={props.onClick}
-      className={`relative flex justify-between w-full max-w-lg mb-4 px-8 py-4 bg-gray-50 text-white text-xl rounded-3xl active:shadow group ${
-        props.connected ? 'opacity-50' : ''
-      }`}
+      className="relative flex justify-between w-full max-w-lg mb-4 px-8 py-4 bg-gray-50 text-white text-xl rounded-3xl active:shadow group"
       style={{ backgroundColor: props.colorCode }}
     >
       <span>
-        {props.icon && !props.connected && <Icon icon={props.icon} />}
+        {props.icon && <Icon icon={props.icon} />}
         {props.children}
       </span>
       <Overlay />
